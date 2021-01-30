0cba57bec53f17bf36688e9487a009706d6ed256
sesath <sesathdecosta@gmail.com>
2021-01-30 08:15:47 +0530

    Rewamp SuperDAO

--- src/main/java/lk/ijse/dep/web/dao/SuperDAO.java
+++ src/main/java/lk/ijse/dep/web/dao/SuperDAO.java
@@ -1,11 +1,9 @@
 package lk.ijse.dep.web.dao;
 
 import javax.persistence.EntityManager;
-import java.sql.Connection;
 
 public interface SuperDAO {
 
   void setEntityManager(EntityManager entityManager) throws Exception;
-  void setConnection(Connection connection) throws Exception;
 
 }
