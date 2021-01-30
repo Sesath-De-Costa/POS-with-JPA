133eb0d6fca841913f2c7cca7d7019fa08c10d9c
sesath <sesathdecosta@gmail.com>
2021-01-30 08:16:52 +0530

    Rewamp CrudDAOImpl

--- src/main/java/lk/ijse/dep/web/dao/CrudDAOImpl.java
+++ src/main/java/lk/ijse/dep/web/dao/CrudDAOImpl.java
@@ -1,60 +1,53 @@
 package lk.ijse.dep.web.dao;
 
 import lk.ijse.dep.web.entity.SuperEntity;
-import org.hibernate.Session;
 
 import javax.persistence.EntityManager;
 import java.io.Serializable;
 import java.lang.reflect.ParameterizedType;
-import java.sql.Connection;
 import java.util.List;
 
 public class CrudDAOImpl<T extends SuperEntity, K extends Serializable> implements CrudDAO<T, K> {
 
-    private EntityManager em;
-    private Class<T> entityClass;
+  private EntityManager em;
+  private Class<T> entityClass;
 
-    public CrudDAOImpl() {
-        entityClass = (Class<T>) (((ParameterizedType) (this.getClass().getGenericSuperclass())).getActualTypeArguments()[0]);
-    }
+  public CrudDAOImpl() {
+    entityClass = (Class<T>) (((ParameterizedType) (this.getClass().getGenericSuperclass())).getActualTypeArguments()[0]);
+  }
 
-    protected EntityManager getEntityManager() {
-        return this.em;
-    }
-
-    @Override
-    public void setEntityManager(EntityManager entityManager) throws Exception {
-        this.em = entityManager;
-    }
-
-  @Override
-  public void setConnection(Connection connection) throws Exception {
-
+  protected EntityManager getEntityManager() {
+    return this.em;
   }
 
   @Override
-    public void save(T entity) throws Exception {
-        em.persist(entity);
-    }
+  public void setEntityManager(EntityManager entityManager) throws Exception {
+    this.em = entityManager;
+  }
 
-    @Override
-    public void update(T entity) throws Exception {
-        em.merge(entity);
-    }
+  @Override
+  public void save(T entity) throws Exception {
+    em.persist(entity);
+  }
 
-    @Override
-    public void delete(K key) throws Exception {
-        em.remove(em.find(entityClass, key));
-    }
+  @Override
+  public void update(T entity) throws Exception {
+    em.merge(entity);
+  }
 
-    @Override
-    public List<T> getAll() throws Exception {
-        return em.createQuery("FROM " + entityClass.getName()).getResultList();
-    }
+  @Override
+  public void delete(K key) throws Exception {
+    em.remove(em.find(entityClass, key));
+  }
 
-    @Override
-    public T get(K key) throws Exception {
-        return em.find(entityClass, key);
-    }
+  @Override
+  public List<T> getAll() throws Exception {
+    return em.createQuery("FROM " + entityClass.getName()).getResultList();
+  }
+
+  @Override
+  public T get(K key) throws Exception {
+    return em.find(entityClass, key);
+  }
 
 }
